f2973a109e237586e14a3877185ac312f44a1ee5
Mikedev <mdv1994@gmail.com>
2022-09-26 23:50:31 +0200

    add discriminator

---
+++ factor_vae/model/discriminator.py
@@ -0,0 +1,20 @@
+from torch import nn
+
+
+def make_sequential_discriminator(input_size: int, num_hidden_layers: int, hidden_size: int) -> nn.Module:
+    """
+    Creates a discriminator MLP network with activation function LeakyReLU.
+    The output layer is a linear layer with output size 1.
+    :param input_size: the input size
+    :param num_hidden_layers: the number of hidden layers
+    :param hidden_size: the size of each hidden layer
+    :return: the discriminator network
+    """
+    layers = []
+    layers.append(nn.Linear(input_size, hidden_size))
+    layers.append(nn.ReLU())
+    for _ in range(num_hidden_layers - 1):
+        layers.append(nn.Linear(hidden_size, hidden_size))
+        layers.append(nn.LeakyReLU())
+    layers.append(nn.Linear(hidden_size, 1))
+    return nn.Sequential(*layers)
