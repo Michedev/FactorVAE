a2acdabac005ec5bca1e46e29608b9b5e36850f8
Mikedev <mdv1994@gmail.com>
2022-09-29 01:23:41 +0200

    add encoder_decoder.py file

---
+++ factor_vae/model/encoder_decoder.py
@@ -0,0 +1,35 @@
+from torch import nn
+
+
+def dsprites_encoder(input_channels: int, latent_size: int):
+    return nn.Sequential(
+        nn.Conv2d(input_channels, 32, kernel_size=4, stride=2),
+        nn.ReLU(),
+        nn.Conv2d(32, 32, kernel_size=4, stride=2),
+        nn.ReLU(),
+        nn.Conv2d(32, 64, kernel_size=4, stride=2),
+        nn.ReLU(),
+        nn.Conv2d(64, 64, kernel_size=4, stride=2),
+        nn.ReLU(),
+        nn.Flatten(),
+        nn.Linear(256, 128),
+        nn.ReLU(),
+        nn.Linear(128, latent_size * 2),
+    )
+
+
+def dsprites_decoder(latent_size: int, output_channels: int):
+    return nn.Sequential(
+        nn.Linear(latent_size, 128),
+        nn.ReLU(),
+        nn.Linear(128, 256),
+        nn.ReLU(),
+        nn.Unflatten(1, (64, 2, 2)),
+        nn.ConvTranspose2d(64, 64, kernel_size=4, stride=2),
+        nn.ReLU(),
+        nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2),
+        nn.ReLU(),
+        nn.ConvTranspose2d(32, 32, kernel_size=4, stride=2),
+        nn.ReLU(),
+        nn.ConvTranspose2d(32, output_channels, kernel_size=3, stride=1),
+    )
