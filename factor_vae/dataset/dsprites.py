67c8b43894405841fae716306c767905eb82645d
Mikedev <mdv1994@gmail.com>
2022-09-29 00:35:27 +0200

    add print preload, add argument dataset len

--- factor_vae/dataset/dsprites.py
+++ factor_vae/dataset/dsprites.py
@@ -13,9 +13,11 @@ class DSpritesImages(Dataset):
     DSprites dataset containing only images
     """
 
-    def __init__(self, train_size: float, train: bool = True, download=True, preload=True):
+    def __init__(self, train_size: float, train: bool = True, download=True, preload=True, dataset_len=737280):
+        assert 737280 >= dataset_len > 0, 'dataset_len must be in range [1, 737280]'
         self.train_size = train_size
         self.train = train
+        self.dataset_len = dataset_len
         dsprites_path = DATA / 'dsprites_ndarray_co1sh3sc6or40x32y32_64x64.hdf5'
         if not dsprites_path.exists():
             if download:
@@ -24,18 +26,19 @@ class DSpritesImages(Dataset):
                     raise RuntimeError('Download failed')
             raise FileNotFoundError('Please download the dataset from {} and place it in {}'
                                     .format('https://github.com/deepmind/dsprites-dataset', DATA))
-        self.dsprites_len = 737280  # took from github repo
-        self.train_len = int(self.dsprites_len * train_size)
-        self.val_len = self.dsprites_len - self.train_len
+        self.train_len = int(self.dataset_len * train_size)
+        self.val_len = self.dataset_len - self.train_len
 
         self.dataset_len = self.train_len if train else self.val_len
         # load hdf5 file
         self.dsprites = h5py.File(dsprites_path, 'r')['imgs']
         if preload:
+            print('Preloading', 'train' if train else 'val', 'dataset')
             if self.train:
                 self.dsprites = self.dsprites[:self.train_len]
             else:
                 self.dsprites = self.dsprites[self.train_len:]
+            print('Done')
 
     def __len__(self):
         return self.dataset_len
