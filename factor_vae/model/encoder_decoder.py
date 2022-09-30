99d530e78e80acd66da036db9e11b0391a72297a
Mikedev <mdv1994@gmail.com>
2022-09-30 18:56:53 +0200

    add layer norm

--- factor_vae/model/encoder_decoder.py
+++ factor_vae/model/encoder_decoder.py
@@ -4,15 +4,20 @@ from torch import nn
 def dsprites_encoder(input_channels: int, latent_size: int):
     return nn.Sequential(
         nn.Conv2d(input_channels, 32, kernel_size=4, stride=2),
+        nn.GroupNorm(1, 32),
         nn.ReLU(),
         nn.Conv2d(32, 32, kernel_size=4, stride=2),
+        nn.GroupNorm(1, 32),
         nn.ReLU(),
         nn.Conv2d(32, 64, kernel_size=4, stride=2),
+        nn.GroupNorm(1, 64),
         nn.ReLU(),
         nn.Conv2d(64, 64, kernel_size=4, stride=2),
+        nn.GroupNorm(1, 64),
         nn.ReLU(),
         nn.Flatten(),
         nn.Linear(256, 128),
+        nn.LayerNorm(128),
         nn.ReLU(),
         nn.Linear(128, latent_size * 2),
     )
@@ -21,15 +26,20 @@ def dsprites_encoder(input_channels: int, latent_size: int):
 def dsprites_decoder(latent_size: int, output_channels: int):
     return nn.Sequential(
         nn.Linear(latent_size, 128),
+        nn.LayerNorm(128),
         nn.ReLU(),
         nn.Linear(128, 1024),
+        nn.LayerNorm(1024),
         nn.ReLU(),
         nn.Unflatten(1, (64, 4, 4)),
         nn.ConvTranspose2d(64, 64, kernel_size=4, stride=2, padding=1),
+        nn.GroupNorm(1, 64),
         nn.ReLU(),
         nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
+        nn.GroupNorm(1, 32),
         nn.ReLU(),
         nn.ConvTranspose2d(32, 32, kernel_size=4, stride=2, padding=1),
+        nn.GroupNorm(1, 32),
         nn.ReLU(),
         nn.ConvTranspose2d(32, output_channels, kernel_size=4, stride=2, padding=1),
     )
